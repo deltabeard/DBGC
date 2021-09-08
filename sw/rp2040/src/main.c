ecb4715ba2cf234c4d56e1b60a25cecac5107a5e
Mahyar Koshkouei <mk@deltabeard.com>
2021-09-08 15:07:34 +0100

    rp2040: add PIO to CMakeLists & fix syntax errors
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- sw/rp2040/src/main.c
+++ sw/rp2040/src/main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#include <pico/stdlib.h>
+#include <hardware/pio.h>
+
+#include "comms.pio.h"
+
+#define PIN_D0	0
+#define PIN_A15	23
+#define PIN_DATADIR 25
+#define PIN_WR	27
+#define PIN_CS	29
 
 int main(void)
 {
+	PIO pio = pio0;
+	uint sm_a15, sm_cs, sm_wr, sm_do;
+
+	gb_bus_program_init(pio, &sm_a15, &sm_cs, &sm_wr, &sm_do,
+			PIN_D0, PIN_A15, PIN_CS, PIN_WR, PIN_DATADIR);
+	sleep_ms(500);
+
 	return 0;
 }
