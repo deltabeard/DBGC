e2fcedee733851e6ebea77278d25e9054e9e62d8
Mahyar Koshkouei <mk@deltabeard.com>
2021-09-28 18:59:33 +0100

    sw: verify: add terminal
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- sw/verify/src/verify.c
+++ sw/verify/src/verify.c
@@ -1,8 +1,11 @@
 #define _GNU_SOURCE
 
-#include <pico/stdio.h>
-#include <pico/bootrom.h>
 #include <stdio.h>
+#include <pico/stdlib.h>
+#include <pico/bootrom.h>
+#include <pico/binary_info.h>
+#include <hardware/i2c.h>
+#include <hardware/clocks.h>
 #include <string.h>
 
 #define arraysize(array)    (sizeof(array)/sizeof(array[0]))
@@ -10,72 +13,123 @@
 struct func_map {
 	char *long_arg;
 	char *help;
-	void (*func)(const char *cmd, const char *arg);
+	void (*func)(const char *cmd);
 };
 
-void func_help(const char *cmd, const char *arg);
-void func_reboot(const char *cmd, const char *arg);
+void func_help(const char *cmd);
+void func_i2cscan(const char *cmd);
+void func_reboot(const char *cmd);
 
 static const struct func_map map[] = {
-	{ "HELP", "Print usage information", func_help },
-	{ "REBOOT", "Reboot to USBBOOT", func_reboot }
+	{ "HELP", "Print usage information",	func_help },
+	{ "I2C SCAN", "Perform I2C bus scan",	func_i2cscan },
+	{ "REBOOT", "Reboot to USBBOOT",	func_reboot },
 };
 
-void func_reboot(const char *cmd, const char *arg)
+// I2C reserves some addresses for special purposes. We exclude these from the scan.
+// These are any addresses of the form 000 0xxx or 111 1xxx
+bool reserved_addr(uint8_t addr)
 {
+	return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+}
+
+void func_i2cscan(const char *cmd)
+{
+	(void) cmd;
+
+	// This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
+	i2c_init(i2c_default, 50 * 1000);
+	gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+	gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+	gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+	gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+	// Make the I2C pins available to picotool
+	bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+
+	printf("\nI2C Bus Scan\n");
+	printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+	for (int addr = 0; addr < (1 << 7); ++addr) {
+		if (addr % 16 == 0) {
+			printf("%02x ", addr);
+		}
+
+		// Perform a 1-byte dummy read from the probe address. If a slave
+		// acknowledges this address, the function returns the number of bytes
+		// transferred. If the address byte is ignored, the function returns
+		// -1.
+
+		// Skip over any reserved addresses.
+		int ret;
+		uint8_t rxdata;
+		if (reserved_addr(addr))
+			ret = PICO_ERROR_GENERIC;
+		else
+			ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+
+		printf(ret < 0 ? "." : "@");
+		printf(addr % 16 == 15 ? "\n" : "  ");
+	}
+	printf("Done.\n");
+	i2c_deinit(i2c_default);
+}
+
+void func_reboot(const char *cmd)
+{
+	(void) cmd;
 	reset_usb_boot(0, 0);
 }
 
-void func_help(const char *cmd, const char *arg)
+void func_help(const char *cmd)
 {
 	(void) cmd;
-	(void) arg;
 
 	puts("Usage:");
 	for(unsigned i = 0; i < arraysize(map); i++)
 	{
-		fputs(map[i].long_arg, stdout);
-		fputs(": \t", stdout);
-		puts(map[i].help);
+		printf("%s: %s\r", map[i].long_arg, map[i].help);
 	}
 }
 
 int main(void)
 {
 	char buf[64];
-	size_t first_arg_len;
+
+	set_sys_clock_48mhz();
 
 	/* If baudrate is set to PICO_STDIO_USB_RESET_MAGIC_BAUD_RATE, then the
 	 * RP2040 will reset to BOOTSEL mode. */
 	stdio_init_all();
 
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "EndlessLoop"
 new_cmd:
-	fputs("CMD> ", stdout);
-	fgets(buf, sizeof(buf), stdin);
-
-	/* Get length of first argument. */
+	printf("CMD> ");
+	for(unsigned i = 0; i < sizeof(buf); i++)
 	{
-		char *end_of_first_arg;
-		end_of_first_arg = strchrnul(buf, ' ');
-		first_arg_len = end_of_first_arg - &buf[0];
+		buf[i] = getchar();
+		putchar(buf[i]);
+		if(buf[i] == '\r')
+		{
+			buf[i] = '\0';
+			break;
+		}
+	}
+
+	if(buf[0] == '\0')
+	{
+		strcpy(buf, "<no input>");
 	}
 
 	for(unsigned i = 0; i < arraysize(map); i++)
 	{
-		int cmp;
-		cmp = strncmp(buf, map[i].long_arg, first_arg_len);
-		if(cmp == 0)
-		{
-			map[i].func(buf, &buf[first_arg_len]);
-			goto new_cmd;
-		}
+		if(strncmp(buf, map[i].long_arg, strlen(map[i].long_arg)) != 0)
+			continue;
+
+		map[i].func(buf);
+		goto new_cmd;
 	}
 
-	puts("Unrecognised command.");
+	printf("Unrecognised command '%s'\n", buf);
 	goto new_cmd;
 
-#pragma clang diagnostic pop
-	__builtin_unreachable();
+	return 0;
 }
